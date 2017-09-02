7323fc7c44a64832f3a80414927b99024bb90fd7
Carlos A Rodriguez Santiago <carlos.rodriguez75@upr.edu>
2017-09-01 20:22:05 -0400

    Initial Commit

---
+++ Lab0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab0
+//
+//  Created by Carlos A. Rodriguez Santiago on 9/1/17.
+//  Copyright © 2017 Carlos A. Rodriguez Santiago. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
